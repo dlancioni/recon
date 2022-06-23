beb2f70a2d0c3170330cb24e2ba5e4c4e0cd8c2a
David Lancioni <dlancioni@gmail.com>
2022-06-23 08:45:37 -0300

    Add recon stup

--- src/reconlib.py
+++ src/reconlib.py
@@ -7,4 +7,37 @@ class ReconLib:
         self.id = id
         self.name = name
         self.logger = logging.getLogger(__name__)
+        
+    def aggregate(self):
+        """ aggregate and group imported data into temporary table """
+        self.method = "reconlib.aggregate()"
+        sql = ""
+        sqlib = SqlLib()
+        
+    def match(self):
+        """ set the id to parent id on the other side  """
+        self.method = "reconlib.match()"
+        sql = ""
+        sqlib = SqlLib()
+        
+    def compare(self):
+        """ compare the records and relegate the status """
+        self.method = "reconlib.match()"
+        sql = ""
+        sqlib = SqlLib()
+        
+    def stamp(self):
+        """ update the final status from grouped tmp table to flat table """
+        self.method = "reconlib.stamp()"
+        sql = ""
+        sqlib = SqlLib()        
 
+    """ reconcile the positions """
+    def reconcile(self, cursor, ds):
+        self.method = "reconlib.reconcile()"
+        sql = ""
+        sqlib = SqlLib()
+        try:
+        except:
+            self.logger.error(f"{self.method}:Last SQL command {sql}")
+            self.logger.error(f"{self.method}:Error importing the file {path}")
