cd4817cbc23756791c203ce41a2b51b35ace6107
David Lancioni <dlancioni@gmail.com>
2022-06-27 07:33:19 -0300

    Creating compare routine

--- src/reconlib.py
+++ src/reconlib.py
@@ -15,6 +15,7 @@ class ReconLib(BaseLib):
         self.tb2 = f"tb{self.id}2"
         self.tmp1 = f"tmp{self.id}1"
         self.tmp2 = f"tmp{self.id}2"
+        self.tmp3 = f"tmp{self.id}3"
 
     def aggregate(self, cn, recon):
         """ aggregate and group imported data into temporary table """
@@ -49,11 +50,30 @@ class ReconLib(BaseLib):
         sql = f"update {self.tmp2} set recon='{self.name}', rule='{rule}', status='Matched', id_parent = (select id from {self.tmp1} where 1 = 1 {matching_key})"
         cn.execute(sql)
 
-    def compare(self):
+    def compare(self, cn, recon):
         """ compare the records and relegate the status """
         self.method = "reconlib.match()"
         sql = ""
         sqllib = SqlLib()
+        rule = recon["Rule"]
+        fields_key = recon["Key"]
+        fields_compare = recon["Compare"]
+        field_list = sqllib.get_field_list(fields_key)
+        matching_key = sqllib.get_sql_key(self.tmp1, self.tmp2, fields_key)
+        tablename = self.tmp3       
+        for field in fields_compare:
+            cn.execute(f"drop table if exists {tablename}")
+            sql = ""
+            tmp1 = f"{self.tmp1}.{field}"
+            tmp2 = f"{self.tmp2}.{field}"
+            sql += f" create table {tablename} as select"
+            sql += f" {field_list}"
+            sql += f", ({tmp1} + '/' + {tmp2}) difference"
+            sql += f", ({tmp1} = {tmp2}) equals"
+            sql += f" from {self.tmp1}, {self.tmp2}"
+            sql += f" where 1 = 1 {matching_key}"
+            sql = sql.lower()
+            cn.execute(sql)
         
     def stamp(self):
         """ update the final status from grouped tmp table to flat table """
@@ -69,6 +89,7 @@ class ReconLib(BaseLib):
             for recon in recons:
                 self.aggregate(cn, recon)
                 self.match_key(cn, recon)
+                self.compare(cn, recon)
         except Error as err:
             self.logger.error(f"{self.method}: Error to manipulate data [{sql}]: {str(err)}")
         except BaseException as err:
