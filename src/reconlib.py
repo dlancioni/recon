0060bddb1d51896518902ec2d3ee91bf2c6d28ba
David Lancioni <dlancioni@gmail.com>
2022-07-05 17:26:34 -0300

    Improve recon logic

--- src/reconlib.py
+++ src/reconlib.py
@@ -18,8 +18,19 @@ class ReconLib(BaseLib):
         self.tmp1 = f"tmp{self.id}1"
         self.tmp2 = f"tmp{self.id}2"
         self.tmp3 = f"tmp{self.id}3"
-        self.field_key = ""
-        self.matching_key = ""
+        self.field_key = []
+        self.field_compare = []
+        
+    def prepare(self, cn, recon):
+        """ keep common information in class level """
+        self.method = "reconlib.prepare()"
+        dblib = DbLib()
+        sqllib = SqlLib()
+        for field in recon["Fields"]:
+            if str(field["Type"]).strip().lower() == "key":
+                self.field_key.append(str(field["Name"]).strip().lower())
+            if str(field["Type"]).strip().lower() == "compare":
+                self.field_compare.append(str(field["Name"]).strip().lower())                
 
     def aggregate(self, cn, recon):
         """ aggregate, group and order imported data into temporary table """
@@ -61,57 +72,52 @@ class ReconLib(BaseLib):
             sql += f"where 1=1 "
             sql += f"{matching_key}"
             dblib.execute(cn, sql)
-        self.matching_key = matching_key
 
     def compare(self, cn, recon):
         """ compare the records and relegate the status from matched to divergent """
-        self.method = "reconlib.match()"
+        self.method = "reconlib.compare()"
         sql = ""
         fields_key = ""
         utillib = UtilLib()
         sqllib = SqlLib()
         dblib = DbLib()
         rule = recon["Rule"]
-        matching_key = self.matching_key
+        matching_key = sqllib.get_sql_key(self.tmp1, self.tmp2, recon["Fields"])
         """ create temp table  to compare fields """
-        for field in recon["Fields"]:
-            if str(field["Type"]).strip().lower() == "key":
-                field_name = str(field["Name"]).strip().lower()
-                fields_key += f"{self.tmp1}.{field_name}, "
+        for field in self.field_key:
+                fields_key += f"{self.tmp1}.{field}, "
         fields_key = fields_key.strip()[:-1].lower()
         """ keep difference and status in tmp3 """
         tablename = self.tmp3
-        for field in recon["Fields"]:
-            if str(field["Type"]).strip().lower() == "compare":
-                sql = f"drop table if exists {tablename}"
+        for field in self.field_compare:
+            sql = f"drop table if exists {tablename}"
+            dblib.execute(cn, sql)
+            sql = ""
+            tmp1 = f"{self.tmp1}.{field}"
+            tmp2 = f"{self.tmp2}.{field}"
+            sql += f" create table {tablename} as"
+            sql += f" select"
+            sql += f" {fields_key}"
+            sql += f", ({tmp1} || '/' || {tmp2}) difference"
+            sql += f", ({tmp1} = {tmp2}) equality"
+            sql += f" from {self.tmp1}, {self.tmp2}"
+            sql += f" where {self.tmp1}.status = 'matched'"
+            sql += f" {matching_key}"
+            dblib.execute(cn, sql)
+            """ stamp the differences in tmp1/tmp2 tables """
+            for side in range(1,3):
+                temps = self.tmp1 if side == 1 else self.tmp2
+                matching_key = sqllib.get_sql_key(temps, self.tmp3, recon["Fields"])
+                sql = f"alter table {temps} add {field}_diff text default ''"
                 dblib.execute(cn, sql)
-                field = field["Name"].lower()
-                sql = ""
-                tmp1 = f"{self.tmp1}.{field}"
-                tmp2 = f"{self.tmp2}.{field}"
-                sql += f" create table {tablename} as"
-                sql += f" select"
-                sql += f" {fields_key}"
-                sql += f", ({tmp1} || '/' || {tmp2}) difference"
-                sql += f", ({tmp1} = {tmp2}) equality"
-                sql += f" from {self.tmp1}, {self.tmp2}"
-                sql += f" where {self.tmp1}.status = 'matched'"
-                sql += f" {matching_key}"
+                sql = ""                    
+                sql += f"update {temps} set "
+                sql += f"status = 'divergent', "
+                sql += f"{field}_diff = {self.tmp3}.difference "
+                sql += f"from {self.tmp3} "
+                sql += f"where {self.tmp3}.equality = 0 "
+                sql += f"{matching_key}"
                 dblib.execute(cn, sql)
-                """ stamp the differences in tmp1/tmp2 tables """
-                for side in range(1,3):
-                    temps = self.tmp1 if side == 1 else self.tmp2
-                    matching_key = sqllib.get_sql_key(temps, self.tmp3, recon["Fields"])
-                    sql = f"alter table {temps} add {field}_diff text default ''"
-                    dblib.execute(cn, sql)
-                    sql = ""                    
-                    sql += f"update {temps} set "
-                    sql += f"status = 'divergent', "
-                    sql += f"{field}_diff = {self.tmp3}.difference "
-                    sql += f"from {self.tmp3} "
-                    sql += f"where {self.tmp3}.equality = 0 "
-                    sql += f"{matching_key}"
-                    dblib.execute(cn, sql)
 
     def stamp(self, cn, recon):
         """ update the final status from grouped tmp table to flat table """
@@ -136,6 +142,7 @@ class ReconLib(BaseLib):
         try:
             recons = setup["Recons"]
             for recon in recons:
+                self.prepare(cn, recon)
                 self.aggregate(cn, recon)
                 self.match_key(cn, recon)
                 self.compare(cn, recon)
