a935ea314a1657b47350994f0f61168be7384973
David Lancioni <dlancioni@gmail.com>
2022-07-07 13:59:31 -0300

    Identation and comments

--- src/corelib.py
+++ src/corelib.py
@@ -11,7 +11,6 @@ from src.arealib import AreaLib
 from src.utillib import UtilLib
 from src.setuplib import SetupLib
 from src.reconlib import ReconLib
-
 fslib = FsLib()
 sqllib = SqlLib()
 utillib = UtilLib()
@@ -31,12 +30,13 @@ class CoreLib(BaseLib):
         setuplib = SetupLib()
         fslib = FsLib()
         try:
+            """ get recon and setup info """
             recon = setuplib.get_recon_info(recon)
             self.id = recon["Id"]
             self.name = recon["Name"]
             app_path = fslib.get_dir_parent(fslib.get_dir())
             setup = fslib.get_json(app_path + "\\setup.json")
-
+            """ create log per recon """
             log_path = setup["log"] 
             if log_path.find("etc:") > -1:
                 log_path = fslib.get_dir_etc()
@@ -47,23 +47,22 @@ class CoreLib(BaseLib):
             logging.basicConfig(filename = log_path, filemode = "w", datefmt='%Y-%m-%d %H:%M:%S', format = log_format, level=logging.DEBUG)
             logger = logging.getLogger()
             utillib.log(f"Running recon {self.name}")
-
+            """ validate json file """
             cn = dblib.begin_tran()
             if not setuplib.validate(recon):
                 self.logger.info(f"{self.method}: Setup is invalid, aborting this recon")
                 return False
-
+            """ create recon area """
             arealib = AreaLib(self.id, self.name)
             fields, types = arealib.process(cn, recon)
-
+            """ import files """
             etllib = EtlLib(self.id, self.name)
             etllib.process(cn, recon)
-
+            """ reconcile data """
             reconlib = ReconLib(self.id, self.name, fields, types)
             reconlib.process(cn, recon)
-
+            """ generate output """
             utillib.print(cn)
-            
         except BaseException as err:
             dblib.rollback_tran(cn)
             self.logger.error(f"{self.method}:Fail to reconcile: {str(err)} ")
