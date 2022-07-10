fbf608ffd118516f1936e07383b3dae2cdc54347
David Lancioni <dlancioni@gmail.com>
2022-07-10 19:33:40 -0300

    Fix session on messages

--- src/setuplib.py
+++ src/setuplib.py
@@ -19,63 +19,74 @@ class SetupLib(BaseLib):
         self.error = ""
         self.logger = logging.getLogger(__name__)
     
-    def validate_tag(self, recon, field, mandatory=False):
+    def validate_tag(self, session, recon, field, mandatory=False):
         """ dynamic validator """                
         field = msglib.get_value(msglib.field, field)
         if not field in recon:
-            raise Exception(msglib.get_value(msglib.validation, "M1", [field]))
+            raise Exception(msglib.get_value(msglib.validation, "M1", [session, field]))
         if mandatory:
             if str(recon[field]).strip() == "":
                 raise Exception(msglib.get_value(msglib.validation, "M2", [field]))
 
     def validate_info(self, recon):
         """ validate key info """
-        self.validate_tag(recon, "ID", True)
-        self.validate_tag(recon, "NAME", True)
-        self.validate_tag(recon, "DESC", True)
+        session = ""
+        self.validate_tag(session, recon, "ID", True)
+        self.validate_tag(session, recon, "NAME", True)
+        self.validate_tag(session, recon, "DESC", True)
         field = msglib.get_value(msglib.field, "ID")
         id = str(recon[field]).strip()
         if int(id) <= 0: raise Exception(msglib.get_value(msglib.validation, "M3", [field]))
         
     def validate_datasource(self, recon):
-        """ validate datasources """
-        self.validate_tag(recon, "DTSC")
+        """ validate datasources """        
+        sds = msglib.get_value(msglib.field, "DTSC")
+        sfd = msglib.get_value(msglib.field, "FLDS")
+        session = sds
+        self.validate_tag(session, recon, "DTSC")
         ds = msglib.get_value(msglib.field, "DTSC")
         datasources = recon[ds]
         for i in range(0, len(datasources)):
+            session = sds
             tag = recon[ds][i]
-            self.validate_tag(tag, "SIDE", True)
-            self.validate_tag(tag, "NAME", True)
-            self.validate_tag(tag, "PATH", False)
-            self.validate_tag(tag, "FILE", True)
-            self.validate_tag(tag, "SEPT", True)
+            self.validate_tag(session, tag, "SIDE", True)
+            self.validate_tag(session, tag, "NAME", True)
+            self.validate_tag(session, tag, "PATH", False)
+            self.validate_tag(session, tag, "FILE", True)
+            self.validate_tag(session, tag, "SEPT", True)
             """ validate fields """
-            self.validate_tag(recon[ds][i], "FLDS")
+            self.validate_tag(session, recon[ds][i], "FLDS")
             field = msglib.get_value(msglib.field, "FLDS")
             fields = recon[ds][i][field]
             for j in range(0, len(fields)):
+                session = f"{sds}/{sfd}"
                 tag = recon[ds][i][field][j]
-                self.validate_tag(tag, "NAME", True)
-                self.validate_tag(tag, "TYPE", True)
-                self.validate_tag(tag, "VLUE", False)
-                self.validate_tag(tag, "MASK", False)
+                self.validate_tag(session, tag, "NAME", True)
+                self.validate_tag(session, tag, "TYPE", True)
+                self.validate_tag(session, tag, "VLUE", False)
+                self.validate_tag(session, tag, "MASK", False)
                 
     def validate_recon(self, recon):
         """ validate conciliations """
-        self.validate_tag(recon, "RECN")
+        src = msglib.get_value(msglib.field, "RECN")
+        sfd = msglib.get_value(msglib.field, "FLDS")        
+        session = ""
+        self.validate_tag(session, recon, "RECN")
         rc = msglib.get_value(msglib.field, "RECN")
         recons = recon[rc]
         for i in range(0, len(recons)):
+            session = msglib.get_value(msglib.field, "RECN")
             tag = recon[rc][i]
-            self.validate_tag(tag, "RULE", True)
+            self.validate_tag(session, tag, "RULE", True)
             """ validate fields """
-            self.validate_tag(recons[i], "FLDS")
+            self.validate_tag(session, recons[i], "FLDS")
             field = msglib.get_value(msglib.field, "FLDS")
             fields = recon[rc][i][field]
             for j in range(0, len(fields)):
+                session = f"{src}/{sfd}"
                 tag = recon[rc][i][field][j]
-                self.validate_tag(tag, "TYPE", True)
-                self.validate_tag(tag, "NAME", True)
+                self.validate_tag(session, tag, "TYPE", True)
+                self.validate_tag(session, tag, "NAME", True)
 
     def validate(self, recon):
         """ full validation, structure and data """
