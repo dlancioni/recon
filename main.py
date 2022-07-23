3e742a6cc2878bf1940cdfdf8437ffc644725878
David Lancioni <dlancioni@gmail.com>
2022-07-23 10:52:19 -0300

    Organize main

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import sys
 import csv
 import logging
 import argparse
-from src.fslib import FsLib
+from datetime import timedelta
+from timeit import default_timer as timer
+from termcolor import colored, cprint
 from src.fslib import FsLib
 from src.msglib import MsgLib
-from datetime import timedelta
 from src.utillib import UtilLib
 from src.corelib import CoreLib
 from src.reportlib import ReportLib
-from timeit import default_timer as timer
-from termcolor import colored, cprint
 
+""" general declaration """
 fslib = FsLib()
 msglib = MsgLib()
 utillib = UtilLib()
+corelib = CoreLib()
 reportlib = ReportLib()
+
 """ control the user input """
 utillib.cls()
 parser = argparse.ArgumentParser()
@@ -25,28 +27,33 @@ parser.add_argument("--f", help="File with conciliation configuration")
 parser.add_argument("--t", nargs='?', const=1, type=int, help="Time elapsed")
 parser.add_argument("--r", nargs='?', const=0, type=int, help="Results on console: [0] Sinthetic, [1] Side 1 [2] Side 2 [12] Side 1 and 2")
 args = parser.parse_args()
+
 """ append file extension if not provided """
 filename = "recon [en-us].cfg" if args.f == None else args.f
+
 """ start processing """
 start = timer()
 msglib.print(msglib.get_value(msglib.console, "M1"))
-corelib = CoreLib()
 status, error, reports = corelib.process(filename)
 end = timer()
+
 """ time elapsed """
 if args.t != None:
     msg = msglib.get_value(msglib.console, "M3")
     msg = msglib.set_time(f"{msg}: {timedelta(seconds=end-start)}")
     msg = colored(msg, "yellow")
     print(msg)
+    
 """ finish processing """
 if status == True:
     print(colored(msglib.set_time(msglib.get_value(msglib.console, "M10", [filename])), "green"))
 else:
     print(colored(msglib.set_time(msglib.get_value(msglib.console, "M11", [filename])), "red"))
     print(colored(msglib.set_time(error), "red"))
+    
 """ all done """
-msglib.print(msglib.get_value(msglib.console, "M2"))    
+msglib.print(msglib.get_value(msglib.console, "M2"))
+
 """ generate reports """    
 if args.r != None:
     if args.c != None:
--- src/baselib.py
+++ src/baselib.py
@@ -5,11 +5,7 @@ from src.msglib import MsgLib
 class BaseLib:
     def __init__(self, tb1=""):
         tb1 = tb1
-        tmp1 = ""
         tb2 = ""
-        tmp2 = ""
-        last_error = ""
-        cn = ""
 
     def tagfv(self, doc, tag_en, tag_pt):
         f, v = "", ""
@@ -23,7 +19,7 @@ class BaseLib:
             f = tag_pt
             v = doc[tag_pt]
         return f, v
-        
+
     def tagf(self, doc, tag_en="", tag_pt="", mandatory=True):
         f, v = self.tagfv(doc, tag_en, tag_pt)
         if mandatory == True:
@@ -33,6 +29,7 @@ class BaseLib:
                 msg = msglib.get_value(msglib.validation, "M5", [msg])
                 raise Exception(msg)
         return f
+
     def tagv(self, doc, tag_en="", tag_pt="", mandatory=True):
         f, v = self.tagfv(doc, tag_en, tag_pt)
         return v
