268279114aabd6438dacf4567a4ba805eaca57ef
David Lancioni <dlancioni@gmail.com>
2022-09-02 16:51:02 -0300

    Expire message

--- src/expirelib.py
+++ src/expirelib.py
@@ -10,7 +10,7 @@ class ExpireLib:
         pass
         
     def expired(self):
-        expire_at = "2022"
+        expire_at = "2023"
         year = str(pendulum.now().format("YYYY")).strip()
         expired = (year == expire_at)
         if expired == True:
