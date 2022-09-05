c75dba036bee078082632696f6770f9cc0e018a0
David Lancioni <dlancioni@gmail.com>
2022-09-05 17:48:45 -0300

    Add empty email class

---
+++ src/maillib.py
@@ -0,0 +1,14 @@
+#https://docs.python.org/3/library/email.examples.html
+
+import os
+from src.msglib import MsgLib
+
+msglib = MsgLib()
+
+class MailLib:
+
+    def __init__(self):
+        pass
+        
+    def send(self):
+        pass
