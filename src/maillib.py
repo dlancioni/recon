ae13d73d70570790f5745c08f728e49ae3bd397e
David Lancioni <dlancioni@gmail.com>
2022-09-06 11:05:01 -0300

    Create MailLib

--- src/maillib.py
+++ src/maillib.py
@@ -1,14 +1,41 @@
-#https://docs.python.org/3/library/email.examples.html
-
 import os
-from src.msglib import MsgLib
+import logging
+import smtplib
+from email.message import EmailMessage
+from src.fslib import FsLib
 
-msglib = MsgLib()
+fslib = FsLib()
 
 class MailLib:
 
     def __init__(self):
-        pass
-        
-    def send(self):
-        pass
+        self.logger = logging.getLogger(__name__)
+    
+    def send(self, to, subject, message):
+        sent = False
+        path = fslib.get_path_config("mail.cfg")
+        info = fslib.open_json(path)
+        server = info["smtp"]
+        from_mail = info["from"]
+        password = info["password"]
+        if password == "":
+            return False
+        try:
+            msg = EmailMessage()
+            msg["Subject"] = subject
+            msg["From"] = from_mail
+            msg["To"] = ", ".join(to)
+            msg.set_content(message)
+            server = smtplib.SMTP(server)
+            server.set_debuglevel(1)
+            server.starttls()
+            server.login(from_mail, password)
+            server.send_message(msg)
+            server.quit()
+            sent = True
+        except BaseException as err:
+            cat = msglib.get("E6")
+            msg = f"{cat} {str(err)}"
+            loglib.log(loglib.ERROR, msg)
+            sent = False
+        return sent
