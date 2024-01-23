a64063ae0ef2f1c6a912de95cd7bba2e555eb67d
David Lancioni <dlancioni@gmail.com>
2024-01-23 17:29:05 +0100

    Date part in filenames

--- src/utillib.py
+++ src/utillib.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import pendulum
 
@@ -22,3 +23,18 @@ class UtilLib:
         year = str(pendulum.now().format("YYYY")).strip()
         valid = (year == "2022")
         return valid
+    
+    def apply_date_pattern(filename):
+        dt = datetime.today()
+        yyyy = dt.strftime('%Y')                #YYYY
+        yy = dt.strftime('%y')                  #YY
+        mmm = dt.strftime('%b')                 #MMM
+        mm = dt.strftime('%m')                  #MM
+        dd = dt.strftime('%d')                  #DD
+        filename = filename.lower()
+        filename = filename.replace("<yyyy>", yyyy)
+        filename = filename.replace("<yy>", yy)
+        filename = filename.replace("<mmm>", yy)
+        filename = filename.replace("<mm>", mm)
+        filename = filename.replace("<dd>", dd)
+        return filename
